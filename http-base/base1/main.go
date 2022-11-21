7385610ea2f68a9cedc4b37c69aa11261f6821c7
amadeus <amadeus@1token.trade>
2022-11-21 22:21:37 +0800

    feat: http-base

---
+++ http-base/base1/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// 设置2个路由，/和/hello，分别绑定 indexHandler 和 helloHandler ， 根据不同的HTTP请求会调用不同的处理函数。
+	http.HandleFunc("/", indexHandler)      // 访问/，响应是Path of URL = /
+	http.HandleFunc("/hello", helloHandler) // /hello的响应则是请求头(header)中的键值对信息
+	// 下面一行是用来启动 Web 服务的
+	// 第一个参数是地址，:9999表示在 9999 端口监听。
+	// 第二个参数则代表处理所有的HTTP请求的实例，nil 代表使用标准库中的实例处理，即基于net/http标准库实现Web框架的入口。
+	log.Fatal(http.ListenAndServe(":9999", nil))
+}
+
+// handler echoes r.URL.Path
+func indexHandler(w http.ResponseWriter, req *http.Request) {
+	_, err := fmt.Fprintf(w, "Path of URL = %q\n", req.URL.Path)
+	if err != nil {
+		return
+	}
+}
+
+// handler echoes r.URL.Header
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	for i, v := range req.Header {
+		_, err := fmt.Fprintf(w, "Header[%q] = %q\n", i, v)
+		if err != nil {
+			continue
+		}
+	}
+}
+
+/*
+测试方法
+➜  TinyGin git:(main) ✗ curl http://localhost:9999/
+Path of URL = "/"
+➜  TinyGin git:(main) ✗ curl http://localhost:9999/hello
+Header["User-Agent"] = ["curl/7.79.1"]
+Header["Accept"] = ["*/ /*"]   这里多加了一个斜杠，防止注释结束
+ */
